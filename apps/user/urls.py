9d6fd3cfde7450c0a90b1f68993b3e63c8364e91
zhanghao <1579397900@qq.com>
2020-04-29 15:33:36 +0800

    修改错误

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^register$', RegisterView.as_view(), name='register'),  # 注册视图类
     url(r'^active/(?P<token>.*)$', ActiveView.as_view(), name='active'),  # 激活处理
     url(r'^login$', LoginView.as_view(), name='login'),  # 登录
-    url(r'^logout$', Logout.as_view(), name='logout'),  # 退出登录
+    url(r'^logout$', LogoutView.as_view(), name='logout'),  # 退出登录
 
     url(r'^$', UserInfoView.as_view(), name='user'),  # 用户中心-信息页
     url(r'^order$', UserOrderView.as_view(), name='order'),  # 用户中心-订单页
